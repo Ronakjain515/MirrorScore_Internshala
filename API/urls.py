b7fe504e54cc4a2956c430aa15010b5c92034b75
Ronakjain515 <Jain@810Jain@810>
2021-01-09 17:45:13 +0530

    Add task API

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import ping
+from .views import ping, task
 urlpatterns = [
     path('ping', ping.as_view()),
-
+    path('', task.as_view()),
 ]
--- API/views.py
+++ API/views.py
@@ -1,7 +1,6 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-
-# Create your views here.
+from datetime import datetime
+from datetime import date
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,3 +9,10 @@ class ping(APIView):
     def get(self, request):
         return Response({"status": "OK"})
 
+
+class task(APIView):
+    def get(self, request):
+        dt_object = datetime.fromtimestamp(int(request.GET.get("date")))
+        if dt_object.date() == date.today():
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
